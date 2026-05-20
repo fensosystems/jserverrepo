c28a0ae5f13ed47a13fcc762516d647d86b765c7
Pascal <fomehpascal@yahoo.com>
2026-05-20 11:23:11 +0000

    Initial commit of Node.js application

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>🚀 Success! Jenkins! Your custom Docker container is alive!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running internally on port ${PORT}`);
+});
